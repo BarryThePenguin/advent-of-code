07364230bde840adaca5dbcaa24256ead7b10a71
Jonathan Haines <jonno.haines@gmail.com>
2023-12-25 00:10:52 +1100

    feature: 2023 day-4

---
+++ src/2023/day-4.ts
@@ -0,0 +1,78 @@
+import {sum} from '../chunk.js';
+import {frequency} from '../frequency.js';
+import {range} from '../range.js';
+import {intersection} from '../set.js';
+
+const digit = /\d+/g;
+const whitespace = /\s+/g;
+
+class Scratchcards {
+	cardSize = new Map<number, number>();
+
+	cardCopies = new Map<number, number[]>();
+
+	constructor(input: string[]) {
+		for (const value of input) {
+			const [winningInput, myNumbersInput] = value.split(' | ');
+			const myNumbers = Array.from(myNumbersInput.matchAll(digit), (m) =>
+				Number(m[0]),
+			);
+			const [cardIdInput, winningNumbersInput] = winningInput.split(': ');
+			const [, cardNumberInput] = cardIdInput.split(whitespace);
+			const winningNumbers = Array.from(
+				winningNumbersInput.matchAll(digit),
+				(m) => Number(m[0]),
+			);
+
+			const cardId = Number(cardNumberInput);
+			const {size} = intersection(new Set(winningNumbers), new Set(myNumbers));
+
+			const start = cardId + 1;
+
+			this.cardSize.set(cardId, size);
+			this.cardCopies.set(cardId, range(start, start + size));
+		}
+	}
+
+	points() {
+		let points = 0;
+
+		for (const size of this.cardSize.values()) {
+			if (size > 0) {
+				points += 2 ** (size - 1);
+			}
+		}
+
+		return points;
+	}
+
+	*collect() {
+		let cards = [...this.cardSize.keys()];
+
+		while (cards.length > 0) {
+			const [cardId] = cards;
+			cards = cards.slice(1);
+
+			if (typeof cardId === 'number') {
+				yield cardId;
+				const copy = this.cardCopies.get(cardId);
+
+				if (copy) {
+					cards = [...copy, ...cards];
+				}
+			}
+		}
+	}
+}
+
+export const partOne = (input: string[]) => {
+	const scratchCards = new Scratchcards(input);
+	return scratchCards.points();
+};
+
+export const partTwo = (input: string[]) => {
+	const scratchCards = new Scratchcards(input);
+	const count = frequency(scratchCards.collect());
+
+	return sum(count.values());
+};
