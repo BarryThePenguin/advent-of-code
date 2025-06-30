673514f81b91e10293dd775c1922bb6584ce9a5a
Jonathan Haines <jonno.haines@gmail.com>
2025-06-30 15:54:17 +1000

    test: more timeout

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,15 +2,16 @@ import {defineConfig} from 'vitest/config';
 
 export default defineConfig({
 	test: {
+		testTimeout: 10_000,
 		coverage: {
 			include: ['src/**/*'],
 			provider: 'v8',
 			thresholds: {
 				autoUpdate: true,
 				branches: 99.08,
-				functions: 95.12,
-				lines: 93.79,
-				statements: 93.79,
+				functions: 93.52,
+				lines: 94.2,
+				statements: 94.2,
 			},
 		},
 	},
